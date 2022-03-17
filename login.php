44ba6a1c2de950f2560b734c0a1fddacf8cf9f93
Ohmmy555 <Supphitan.p@kkumail.com>
2022-03-18 03:56:47 +0700

    Update Database and errors

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ include('server.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Check IT Login</title>
-    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="./css/login.css">
    
 </head>
 
@@ -46,7 +46,7 @@ include('server.php');
     </div>
     <div>
         <div id="login-picture">
-            <img src="/picture/pic/search-login.jpg" alt="รูปปุ่มเสิร์ท" class="search-icon">
+            <img src="./picture/pic/search-login.jpg" alt="รูปปุ่มเสิร์ท" class="search-icon">
         </div>
     </div>
 
--- login_db.php
+++ login_db.php
@@ -2,30 +2,30 @@
     session_start();
     include('server.php');
     $errors = array();
-    if(isset($_POST['login_user'])){
+    //if(isset($_POST['login_user'])){
         $username =mysqli_real_escape_string($conn,$_POST['username']);
-        $password=mysqli_real_escape_string($conn,$_POST['password']);
+        $password_1=mysqli_real_escape_string($conn,$_POST['password']);
 
-        if(empty($username)){
+        /*if(empty($username)){
             array_push($errors,"username is required");
           }
           if(empty($password)){
             array_push($errors,"password is required");
-          }
+          }*/
           if(count($errors) == 0){
             $password = md5($password_1);
             $sql = "SELECT * FROM ta WHERE username = '$username' AND password = '$password' ";
-            $result = mysqli_query($conn,$query);
+            $result = mysqli_query($conn,$sql);
             if(mysqli_num_rows($result)==1){
                 $_SESSION['username']=$username;
                 $_SESSION['success']="You are now logged in";
-                header("location: index.php");
+                echo "<script>location.replace('./index.php');</script>";
             }else{
                 array_push($errors,"Wrong username or password combination");
                 $_SESSION['error']="Wrong username or password try again";
-                header("location: index.php");
+                echo "<script>location.replace('./login.php');</script>";
             }
     }
-}
+//}
 
 ?>
