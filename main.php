7f24cb9e8ba0ad4feb0005594c927a7b02e4a5af
Pechmanee <101730587+Pechmanee@users.noreply.github.com>
2022-03-18 04:18:50 +0700

    update main before login
    อัพเดตหน้าแรกก่อนล็อคอิน

---
+++ main.php
@@ -0,0 +1,46 @@
+<?php include('server.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check IT</title>
+    <link rel="stylesheet" href="/css/main.css">
+    
+</head>
+<body>
+    <div id="header-bar">
+        <h1>Check IT</h1>
+        <div id="login-bar">
+            <div class="menu-login">
+                <a href="">
+                    <img src="/picture/icon/user.png" alt="ไอคอนล็อคอิน" class="login-icon">
+                    <p >Admin login</p>
+                </a>
+                
+            </div>
+            <div class="menu-login">
+                <a href=""  >
+                    <img src="/picture/icon/user.png" alt="ไอคอนล็อคอิน" class="login-icon">
+                    <p>TA login</p>
+                </a>
+                
+            </div>
+        </div>
+    </div>
+
+    <div id="paragraph">
+        <div id="block-paragraph">
+            <h2>Help Teaching assistants, and your students.</h2>
+            <p>Web helper for an Teaching assistant’s include grading, attendance, and report.</p>
+        </div>
+        <div id="login-picture">
+            <img src="/picture/pic/search-login.jpg" alt="รูปปุ่มเสิร์ท" class="search-icon">
+        </div>
+    </div>
+
+</body>
+</html>
