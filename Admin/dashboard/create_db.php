c8c341f2ce1c41ba6249582d1bfa4547d061544f
themumumimi <wannakan.m@kkumail.com>
2022-03-20 05:21:17 +0700

    Update create_db.php

--- Admin/dashboard/create_db.php
+++ Admin/dashboard/create_db.php
@@ -5,7 +5,7 @@ session_start();
 
 if (isset($_SESSION['username'])) {
 $username=$_SESSION['username'];
-  $Admin = mysqli_fetch_array(mysqli_query($conn, "SELECT idAdmin FROM admin WHERE Admin_username='$username'"));
+$Admin = mysqli_fetch_array(mysqli_query($conn, "SELECT idAdmin FROM admin WHERE Admin_username='$username'"));
 $query_subj = mysqli_query($conn, "SELECT * FROM Subject WHERE Subject.idAdmin='$Admin[0]'");
 while ($row = mysqli_fetch_array($query_subj)) {
     echo '
