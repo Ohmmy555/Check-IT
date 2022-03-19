15afedd257b68708cefc97b7ea37bebf7929a50e
themumumimi <wannakan.m@kkumail.com>
2022-03-20 05:00:41 +0700

    เปิด/สร้างรายวิชา

--- Admin/dashboard/create.php
+++ Admin/dashboard/create.php
@@ -106,7 +106,7 @@
   </div>
 
 
-  ?>
+
   <div class="action">
     <div class="profile" onclick="menuToggle();">
       <img src="img/test.jpg" alt="profile-pic" class="profile-pic">
@@ -123,28 +123,9 @@
 
   <div class="content">
     <h2>วิชาที่สร้าง</h2>
+
     <?php
-    include('conn.php');
-    session_start();
-    if (isset($_SESSION['username'])) {
-      $Admin = mysqli_fetch_array(mysqli_query($conn, "SELECT idAdmin FROM admin WHERE Admin_username=$username"));
-      $query_subj = mysqli_query($conn, "SELECT * FROM Subject JOIN enroll1 ON(subject.idSubject=enroll1.idSubject) JOIN ta ON(admin.idAdmin=enroll1.idAdmin) JOIN ta ON(ta.idTA=enroll1.idTA) JOIN term ON(term.idterm=enroll1.idterm) WHERE enroll1.idAdmin='$Admin[0]'");
-      while ($row = mysqli_fetch_array($query_subj)) {
-        echo '<div class="detail1"> 
-  
-          <div class="text">
-            <img class="roundpic" src="img/E4eLarNVEAM7n4T.jfif" alt="pic">
-          </div>
-          <div class="content1">
-            <p><span>วิชา :' . $row['Subject_name'] . '</p>
-            <p><span>ผู้สอน : </span> ' . $row['TA_fname'] . $row['TA_lname'] . '</p>
-            <p><span>ปีการศึกษา : </span> ' . $row['idyear'] . ' &nbsp; <span>ภาคเรียน : </span>' . $row['term_num'] . '</p>
-          </div>
-        </div>
-    
-        <div id="line"></div>';
-      }
-    }
+    include('./create_db.php');
     ?>
 
     <!-- <div class="detail2">
