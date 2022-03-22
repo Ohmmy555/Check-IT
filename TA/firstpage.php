36749b4db214dc5cc3bfae587556c374c0cf5d99
Ohmmy555 <Supphitan.p@kkumail.com>
2022-03-22 18:56:49 +0700

    update

--- TA/firstpage.php
+++ TA/firstpage.php
@@ -88,10 +88,12 @@ include('../Databast/database.php');
           <b>Latest Subject</b><br><hr>
 <?php
 $stdid = $_SESSION['stdid'];
-$sql = "SELECT Subject.Subject_name,Subject_detail.idSubject,Subject_detail.section 
-FROM Subject_detail 
-JOIN Subject ON (Subject_detail.idSubject = Subject.idSubject) 
-WHERE (SELECT idSubject FROM TA_has_Subject WHERE idTA ='$stdid')";
+$sql = "SELECT Subject.Subject_name,Teacher.Teacher_name,Term.year,Term.term_num,TA_has_Subject.section 
+    FROM TA_has_Subject JOIN Subject_detail ON (TA_has_Subject.idSubject = Subject_detail.idSubject) 
+    JOIN Subject ON (Subject_detail.idSubject = Subject.idSubject) 
+    JOIN Teacher ON (Subject_detail.idTeacher = Teacher.idTeacher) 
+    JOIN Term ON (Subject_detail.idTerm = Term.idTerm) 
+    WHERE TA_has_Subject.idTA = '$stdid'";
 $result = mysqli_query($conn, $sql);
 foreach($result as $data){
 ?>
